15f4bf8b92734610b152845a719788d451a4d4a7
mariasazhina <msazhina23@gmail.com>
2024-06-02 15:56:30 +0300

    add configs

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mailing.urls'))
-]
+    path('', include('mailing.urls')),
+    path('users/', include('users.urls', namespace='users')),
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ users/management/commands/csu.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@mailing.com',
+            first_name='Admin',
+            last_name='Catalog',
+            is_staff=True,
+            is_superuser=True
+        )
+
+        user.set_password('123456qwerty')
+        user.save()
